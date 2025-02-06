16496ac494dcab4886d79ecb2e971f633969e866
“github <github>
2025-02-06 19:08:15 +0900

    별 레이아웃 작업 20250206

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './styles/common.css'
+import MainPage from './pages/MainPage'
+import Navber from './components/shared/Navber'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-   return <>test</>
+   return (
+      <>
+         <Navber />
+         <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/category/:id" element={<MainPage />} />
+            <Route path="/hot" element={<MainPage />} />
+            <Route path="/new" element={<MainPage />} />
+            <Route path="/end" element={<MainPage />} />
+            <Route path="/comming" element={<MainPage />} />
+            <Route path="/follow" element={<MainPage />} />
+         </Routes>
+      </>
+   )
 }
 
 export default App
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material'
+
+function MainPage() {
+   return (
+      <>
+         <Container maxWidth="md"></Container>
+      </>
+   )
+}
+
+export default MainPage
