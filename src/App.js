d4d0d068da25fdc1f0765b2ad60b2c755cb106b7
“github <github>
2025-02-06 19:31:17 +0900

    up

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import './styles/common.css'
 import Home from './pages/Home'
 
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-      </Routes>
-    </>
-  )
+   return (
+      <>
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+         </Routes>
+      </>
+   )
 }
 
 export default App
