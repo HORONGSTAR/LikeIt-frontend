b510b1bce7a2fcf0c012921e869f3aa1a1cd55e0
“github <github>
2025-02-13 18:05:54 +0900

    별/나은 0213 App병합

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './styles/common.css'
 import { Route, Routes, Link, useLocation } from 'react-router-dom'
 import { Button } from '@mui/material'
+
 import Home from './pages/Home'
 import HotPage from './pages/list/HotPage'
+import NewPage from './pages/list/NewPage'
+import EndPage from './pages/list/EndPage'
+import CommingPage from './pages/list/CommingPage'
+import SearchPage from './pages/list/SearchPage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import CommonSignupPage from './pages/CommonSignupPage'
@@ -14,7 +19,9 @@ import FundingLayout from './components/funding/FundingLayout'
 import FundingTimeline from './components/funding/FundingTimeline'
 import FundingOverview from './components/funding/FundingOverview'
 import StudioPage from './pages/StudioPage'
-import CommunityDetail from './components/community/CommunityDetail'
+import ProjectWritePage from './pages/ProjectWritePage'
+import StudioEditPage from './pages/StudioEditPage'
+import StudioCreatePage from './pages/StudioCreatePage'
 
 function App() {
    const location = useLocation()
@@ -23,9 +30,10 @@ function App() {
       '/signup': true,
       '/commonsignup': true,
       '/studio': true,
-      '/studio/community': true,
-      '/studio/community/write': true,
-      '/studio/community/:id': true,
+      '/protect/write': true,
+      '/community/write': true,
+      '/studio/create': true,
+      '/studio/edit': true,
    }
 
    const dontNeedNavber = pageName[location.pathname]
@@ -36,21 +44,23 @@ function App() {
          <Routes>
             <Route path="/desinguide" element={<DesignGuide />} />
             <Route path="/" element={<Home />} />
-            <Route path="/list/hot" element={<HotPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/commonsignup" element={<CommonSignupPage />} />
+            <Route path="/search" element={<SearchPage />} />
             <Route path="/category/:id" element={<Home />} />
-            <Route path="/hot" element={<Home />} />
-            <Route path="/new" element={<Home />} />
-            <Route path="/end" element={<Home />} />
-            <Route path="/comming" element={<Home />} />
+            <Route path="/hot" element={<HotPage />} />
+            <Route path="/new" element={<NewPage />} />
+            <Route path="/end" element={<EndPage />} />
+            <Route path="/comming" element={<CommingPage />} />
             <Route path="/follow" element={<Home />} />
-            <Route path="/studio" element={<StudioPage />}>
-               <Route path="community" element={<CommunityTab />} />
-               <Route path="community/write" element={<CommunityWritePage />} />
-               <Route path="community/:id" element={<CommunityDetail />} />
-            </Route>
+            <Route path="/studio" element={<StudioPage />} />
+            <Route path="/studio/:id" element={<StudioPage />} />
+            <Route path="/protect/write" element={<ProjectWritePage />} />
+            <Route path="/community/write'" element={<CommunityWritePage />} />
+            <Route path="/studio/create" element={<StudioCreatePage />} />
+            <Route path="/studio/edit/:id" element={<StudioEditPage />} />
+            <Route path="/studio/community/write" element={<CommunityWritePage />} />
             <Route path="/funding" element={<FundingLayout />}>
                <Route path="detail" element={<FundingOverview />} />
                <Route path="timeline" element={<FundingTimeline />} />
